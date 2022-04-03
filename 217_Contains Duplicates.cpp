5edb72d7c2236c846fcd66e280b47edbe9eb931c
Varad Kulkarni <kvarad6@gmail.com>
2022-04-03 15:42:32 +0530

    Solution for Contains Duplicates

---
+++ 217_Contains Duplicates.cpp
@@ -0,0 +1,22 @@
+
+// 217. Contains Duplicate
+
+
+
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        int size = nums.size();
+        sort(nums.begin(), nums.end());
+        for(int i=0;i<size-1;i++)
+        
+            if(nums[i] == nums[i+1])
+            {
+                return true;
+                break;
+            }
+            
+        return false;
+     
+    }
+};
