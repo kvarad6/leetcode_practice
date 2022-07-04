aef6f965cbf7b5747f803415f9714d0d414bc0fc
Varad Kulkarni <56363407+kvarad6@users.noreply.github.com>
2022-07-04 23:29:00 +0530

    Update 217_Contains Duplicates.cpp

--- 217_Contains Duplicates.cpp
+++ 217_Contains Duplicates.cpp
@@ -6,17 +6,30 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        int size = nums.size();
+        /*
+        // Using sorting..(Time Complexity: nlog(n))
         sort(nums.begin(), nums.end());
-        for(int i=0;i<size-1;i++)
         
-            if(nums[i] == nums[i+1])
-            {
+        for(int i=0;i<nums.size()-1;i++){
+            if(nums[i]==nums[i+1]){
                 return true;
-                break;
             }
-            
+        }
         return false;
-     
+        */
+        
+        //Using Map..(Time Complexity: O(n))
+        
+        map<int, int> m;
+        
+        for(int i=0;i<nums.size();i++){
+            if(m.count(nums[i])==0){
+                m[nums[i]]++;
+            }
+            else{
+                return true;
+            }
+        }
+        return false;
     }
 };
