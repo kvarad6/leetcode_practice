a8a5e144cda69866e9aabfa43018fbdfe09507c9
Varad Kulkarni <56363407+kvarad6@users.noreply.github.com>
2022-07-04 23:27:40 +0530

    Create 566_Reshape the Matrix.cpp

---
+++ 566_Reshape the Matrix.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<vector<int>> matrixReshape(vector<vector<int>>& mat, int r, int c) {
+        
+        int current_rows = mat.size();
+        int current_columns = mat[0].size();
+        
+        int total_elements = current_rows * current_columns;
+        
+        vector<vector<int>> modified_mat(r, vector<int>(c));
+        
+        int starting_row = 0;
+        int starting_column = 0;
+        
+        if(total_elements == r*c){
+            for(int i=0;i<current_rows;i++){
+                for(int j=0;j<current_columns;j++){
+                    modified_mat[starting_row][starting_column] = mat[i][j];
+                    starting_column += 1;
+                    if(starting_column == c){
+                        starting_column = 0;
+                        starting_row += 1;
+                    }
+                }
+            }
+            return modified_mat;
+        }
+     return mat;
+    }
+};
