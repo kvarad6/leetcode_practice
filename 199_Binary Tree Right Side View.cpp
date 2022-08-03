4f47917010d4465c211d46f07d65539380262d05
Varad Kulkarni <56363407+kvarad6@users.noreply.github.com>
2022-08-03 09:26:51 +0530

    Create 199_Binary Tree Right Side View.cpp

---
+++ 199_Binary Tree Right Side View.cpp
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+   void recursive_traversal(TreeNode* root, int level, vector<int> &res){
+        
+        if(root==NULL){
+            return;
+        }
+        
+        if(res.size() == level){
+            res.push_back(root->val);
+             }
+        recursive_traversal(root->right, level+1, res);
+        recursive_traversal(root->left, level+1, res);
+       
+    }
+    
+    vector<int> rightSideView(TreeNode* root) {
+        vector<int> res;
+        int level = 0;
+        recursive_traversal(root, level, res);
+        return res;
+    }
+     
+   
+};
