b54204107d12910853db85c38e8e66685813fd54
Varad Kulkarni <56363407+kvarad6@users.noreply.github.com>
2023-08-03 23:46:13 +0530

    Update 509_Fibonacci Number.cpp | added bottom up approach

--- 509_Fibonacci Number.cpp
+++ 509_Fibonacci Number.cpp
@@ -1,4 +1,5 @@
 class Solution {
+    //Top Down Approach (Recursion + Memoisation)
     int fibCalculate(int n, vector<int> &dp){
         if(n<=1){
             return n;
@@ -21,4 +22,19 @@ public:
         answer = fibCalculate(n, dp);
         return answer;
     }
+    //Bottom Up Approach (Tabulation)
+    public:
+        int fib(int n){
+            vector<int> dp(n+1, -1);
+            if(n<=1){
+                return n;
+            }
+            dp[0] = 0;
+            dp[1] = 1;
+            for(int i=2;i<=n;i++){
+                dp[i] = dp[i-1] + dp[i-2];
+            }
+            return dp[n];
+        }
+
 };
