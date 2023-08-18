8ded295a39df82d29f94a237269f757d43d38341
Varad Kulkarni <56363407+kvarad6@users.noreply.github.com>
2023-08-18 09:40:38 +0530

    Create 152_Maximum Product Subarray.cpp | leetcode | striver sheet

---
+++ 152_Maximum Product Subarray.cpp
@@ -0,0 +1,47 @@
+//Bruit force approach | O(n^2)
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        int n = nums.size();
+        int max_product = INT_MIN;
+        int current_product;
+
+        for(int i=0;i<n;i++){
+            current_product = 1;
+            for(int j=i;j<n;j++){
+                current_product *= nums[j];
+                if(current_product>max_product){
+                    max_product = current_product;
+                }
+            }
+        }
+        return max_product;
+    }
+};
+
+//optimised approach | DP
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        int n = nums.size();
+        int prefix = 1;
+        int suffix = 1;
+        int maxProduct = INT_MIN;
+        int currentProduct;
+
+        for(int i=0;i<n;i++){
+            if(prefix==0){
+                prefix = 1;
+            }
+            if(suffix==0){
+                suffix = 1;
+            }
+            prefix = prefix * nums[i];
+            suffix = suffix * nums[n-i-1];
+            currentProduct = max(prefix, suffix);
+            maxProduct = max(maxProduct, currentProduct);
+
+        }
+        return maxProduct;
+    }
+};
