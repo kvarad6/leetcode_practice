f0461c999045a156e9625673c83dffa2b5b6d8f8
Varad Kulkarni <56363407+kvarad6@users.noreply.github.com>
2023-08-28 23:30:11 +0530

    Update Cut into segments.cpp | Added Tabulation Method

--- Cut into segments.cpp
+++ Cut into segments.cpp
@@ -72,3 +72,26 @@ int cutSegments(int n, int x, int y, int z) {
 	return ans;
 }
 
+// Using DP | Bottom Up Appraoch | Tabulation
+
+int solve(int n, int x, int y, int z, vector<int> &dp){
+	//base cases
+	dp[0] = 0;
+	for(int i=1;i<=n;i++){
+		if(i-x>=0) dp[i] = max(dp[i], dp[i-x] + 1);
+		if(i-y>=0) dp[i] = max(dp[i], dp[i-y] + 1);
+		if(i-z>=0) dp[i] = max(dp[i], dp[i-z] + 1);
+	}	
+	return dp[n];
+}
+
+int cutSegments(int n, int x, int y, int z) {
+	vector<int> dp(n+1, INT_MIN);
+	int ans = solve(n, x, y, z, dp);
+	if(ans<0){
+		return 0;
+	}
+	return ans;
+	
+}
+
