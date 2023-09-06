17184bc5df8456f14954e95f1059df35f79cd750
Varad Kulkarni <56363407+kvarad6@users.noreply.github.com>
2023-09-07 00:51:31 +0530

    Update 102_Binary Tree Level Order Traversal.cpp | Added comments and TC/SC

--- 102_Binary Tree Level Order Traversal.cpp
+++ 102_Binary Tree Level Order Traversal.cpp
@@ -12,29 +12,48 @@
 class Solution {
 public:
     vector<vector<int>> levelOrder(TreeNode* root) {
+        //Declaring a vector of vector to store the level wise transversal
         vector<vector<int>> ans;
 
+        //Declaring a queue to store the root node and it's childrens
+        queue<TreeNode*> q;
+
+        //base case 
         if(root==NULL){
             return ans;
         }
-        queue<TreeNode*> q;
+        
+        //At start, push root node in the queue
         q.push(root);
+
+        // transversing the list
         while(!q.empty()){
             int size = q.size();
+
+            //Declaring a level vector to store the nodes that belongs to same level/ parent
             vector<int> level;
-            for(int i=0; i<size; i++){
+
+            for(int i=0;i<size;i++){
                 TreeNode* node = q.front();
                 q.pop();
+                //If left child exists, put it into the queue
                 if(node->left!=NULL){
                     q.push(node->left);
                 }
+                //If right child exists, put it into the queue
                 if(node->right!=NULL){
                     q.push(node->right);
                 }
+
+                //Put the node / current root node into the level vector
                 level.push_back(node->val);
             }
-            ans.push_back(level);
+            //Put the current level/ level vector into the ans vector of vector
+           ans.push_back(level);
         }
         return ans;
     }
 };
+
+//TC: O(N) | As we are transversing all the nodes inside a queue
+//SC: O(N) | As, at worst case, queue is gonna store all the nodes
