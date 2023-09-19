bf20a51b5104546c10717f4070201efb4b6ee19b
Varad Kulkarni <56363407+kvarad6@users.noreply.github.com>
2023-09-19 23:02:26 +0530

    Create Bottom View Of Binary Tree.cpp | Tree | Codestudio

---
+++ Bottom View Of Binary Tree.cpp
@@ -0,0 +1,47 @@
+/************************************************************
+
+    Following is the TreeNode class structure
+
+    template <typename T>
+    class TreeNode {
+       public:
+        T data;
+        TreeNode<T> *left;
+        TreeNode<T> *right;
+
+        TreeNode(T data) {
+            this->data = data;
+            left = NULL;
+            right = NULL;
+        }
+    };
+
+************************************************************/
+
+
+#include<map>
+vector<int> bottomView(TreeNode<int> * root){
+    vector<int> ans;
+    map<int, int> mpp;
+    queue<pair<TreeNode<int>*, int>> q;
+    q.push({root, 0});
+    while(!q.empty()){
+        auto it = q.front();
+        q.pop();
+        TreeNode<int> * node = it.first;
+        int line = it.second;
+        mpp[line] = node->data;
+
+        if(node->left!=NULL){
+          q.push({node->left, line - 1});
+        }
+        if(node->right!=NULL){
+          q.push({node->right, line + 1});
+        }
+    }
+    for (auto it : mpp) {
+        ans.push_back(it.second);
+    }
+    return ans;
+
+}
