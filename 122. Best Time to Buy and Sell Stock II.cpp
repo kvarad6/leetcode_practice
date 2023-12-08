73e52dfbf50a28e93aa105ad9e1f190d76a6a065
Varad Kulkarni <56363407+kvarad6@users.noreply.github.com>
2023-12-08 23:55:21 +0530

    Update 122. Best Time to Buy and Sell Stock II.cpp | leetcode | arrays

--- 122. Best Time to Buy and Sell Stock II.cpp
+++ 122. Best Time to Buy and Sell Stock II.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
+        //using normal approach | minPrice, maxProfit, totalProfit
         int minPrice = prices[0];
         int maxProfit = 0;
         int totalProfit = 0;
@@ -18,5 +19,26 @@ public:
             }
         }
         return totalProfit;
+
+        //using peak-valley concept
+        //buy at valley, sell at peak
+
+        int i=0, profit=0, buy, sell;
+        while(i < prices.size()-1){
+            //if curent price of stock is less than next one, buy...
+            while(i < prices.size()-1 && prices[i] >= prices[i+1]){
+                i++;
+            }
+            buy = prices[i];
+
+            //if current price of stock is more than next one, sell..
+            while(i < prices.size()-1 && prices[i] < prices[i+1]){
+                i++;
+            }
+            sell = prices[i];
+            profit += sell - buy;
+        }
+        return profit;
+
     }
 };
