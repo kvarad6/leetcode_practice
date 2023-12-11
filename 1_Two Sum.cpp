a8c7a299df2f9bb127b74e4b61ff45a6e7a15bb1
Varad Kulkarni <56363407+kvarad6@users.noreply.github.com>
2023-12-12 01:01:59 +0530

    Update 1_Two Sum.cpp

--- 1_Two Sum.cpp
+++ 1_Two Sum.cpp
@@ -7,11 +7,13 @@ public:
         //vector<int> ans;
         
         for(int i=0;i<nums.size();i++){
+        //if for nums[i], target-nums[i] also present, return both..
            if(mpp.find(target-nums[i])!=mpp.end()){
                // ans.push_back(mpp[target-nums[i]]);
                // ans.push_back(i);
                return {mpp[target-nums[i]], i};
            }
+             //otherwise add the element and its index..
            else{
                mpp[nums[i]] = i;
            }
