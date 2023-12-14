99784224d65cf5ff6724c88dcbef73371fe736a0
Varad Kulkarni <56363407+kvarad6@users.noreply.github.com>
2023-12-15 00:31:52 +0530

    Update 21_Merge Two Sorted Lists.cpp | Leetcode | Linked List

--- 21_Merge Two Sorted Lists.cpp
+++ 21_Merge Two Sorted Lists.cpp
@@ -10,75 +10,76 @@
  */
 class Solution {
 public:
-    
-    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
-      
-        //Recursive Approach..
-        ListNode* result;
-        
-        if(l1==NULL){
-            return l2;
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+
+        //------------------ Approach 1 | Iterative Solution --------//
+
+        //base cases..
+        if(list1==NULL){
+            return list2;
         }
-        if(l2==NULL){
-            return l1;
+        if(list2==NULL){
+            return list1;
         }
-        
-        if(l1->val<=l2->val){
-            result = l1;
-            result->next = mergeTwoLists(l1->next, l2);
-        }
-        else
-        {
-            result = l2;
-			result->next = mergeTwoLists(l1, l2 -> next);
-		}
-        return result;
-    }
-    
-    /*
-    //Iterative Approach...
-    
-    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
-        if(l1==NULL){
-            return l2;
-        }
-        if(l2==NULL){
-            return l1;
-        }
-    
-        ListNode* list1 = l1;
-        ListNode* list2 = l2;
-    
+
+        //temp variables pointing to current LLs..
+        ListNode* l1 = list1;
+        ListNode* l2 = list2;
+
+        //creating a new node and assigning it to new LL..
+
         ListNode* dummyNode = new ListNode(-1);
         ListNode* list3 = dummyNode;
-    
-        while(list1!=NULL && list2!=NULL){
-            
-            if(list1->val<=list2->val){
-                list3->next = list1;
-                list1 = list1->next;
+
+        while(l1!=NULL && l2!=NULL){
+            if(l1->val <= l2->val){
+                list3->next = l1;
+                l1 = l1->next;
             }
             else{
-                list3->next = list2;
-                list2 = list2->next;
+                list3->next = l2;
+                l2 = l2->next;
             }
             list3 = list3->next;
         }
-    
-        //For remaining nodes..
-    
-        while(list1!=NULL){
-            list3->next = list1;
-            list1 = list1->next;
-            list3 = list3->next;   
+
+        //Fill out the remaining nodes in new LL..
+
+        while(l1!=NULL){
+            list3->next = l1;
+            list3 = list3->next;
+            l1 = l1->next;
         }
-    
-        while(list2!=NULL){
-            list3->next = list2;
-            list2 = list2->next;
-            list3 = list3->next;   
+
+        while(l2!=NULL){
+            list3->next = l2;
+            list3 = list3->next;
+            l2 = l2->next;
         }
-        return dummyNode->next;
+        
+    return dummyNode->next;
+
+
+    //------------------- Approach 2 | Recursive Solution ----------------//
+
+    //base cases..
+    if(list1==NULL){
+        return list2;
     }
-    */
+    if(list2==NULL){
+        return list1;
+    }
+
+    ListNode* result;
+
+    if(list1->val <= list2->val){
+        result = list1;
+        result->next = mergeTwoLists(list1->next, list2);
+    }
+    else{
+        result = list2;
+        result->next = mergeTwoLists(list1, list2->next);
+    }
+    return result;
+    }
 };
