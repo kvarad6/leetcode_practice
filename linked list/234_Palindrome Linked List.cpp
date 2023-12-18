28dc9829a80ee340014082f763cac90c0a6cdec6
Varad Kulkarni <56363407+kvarad6@users.noreply.github.com>
2023-12-18 23:46:42 +0530

    Update 234_Palindrome Linked List.cpp | Leetcode | LL

--- linked list/234_Palindrome Linked List.cpp
+++ linked list/234_Palindrome Linked List.cpp
@@ -10,52 +10,94 @@
  */
 class Solution {
 public:
+
     bool isPalindrome(ListNode* head) {
-        
-        //Find the middle element using slow-fast pointers
-        
-        ListNode* slow = head;
-        ListNode* fast = head;
-        
-        while(fast->next!=NULL && fast->next->next!=NULL){
-            slow = slow->next;
-            fast = fast->next->next;
-        }
-        //Handling edge cases
-        if(head==NULL || head->next==NULL){
-            return true;
-        }
-        
-        //Reverse the LL next to the middle element
-        slow->next = reverseLL(slow->next);
-        
-        //Compares the nodes from original LL and reversed part
-        slow = slow->next;
+
+        //---------------- Approach 1 -----------------------//
+        // using stack
+        //TC: O(2N)
+        //SC: O(N) | stack
+
+        stack<int> st;
         ListNode* temp = head;
-        
-        while(slow!=NULL){
-            if(temp->val!=slow->val){
+
+        //inserting all the list elements in stack
+
+        while(temp!=NULL){
+            st.push(temp->val);
+            temp = temp->next;
+        }
+
+        //comparing ll elements with stack.
+        //if both same -> palidrome
+        //if not -> not a palidrome
+
+        temp = head;
+        while(temp!=NULL){
+            if(temp->val!=st.top()){
                 return false;
             }
-        slow = slow->next;
-        temp = temp->next;
+            temp = temp->next;
+            st.pop();
         }
-        return true;        
-    }
+        return true;
+
+        //----------------------- Approach 2 -------------------------//
+        // Middle of LL -> Reverse Second Half -> Compare both the halves..
+        // if (element)first half == (element)second half -> palidrome
+
+        //Finding middle of ll | slow-fast
+        //TC: O(2N)
+        //SC: O(1)
+
+        ListNode* fast = head;
+        ListNode* slow = head;
+
+        while(fast->next!=NULL && fast->next->next!=NULL){
+            fast = fast->next->next;
+            slow = slow->next;
+        }
+
+        ListNode* newHead = reverseLL(slow->next);
+
+        //comparing both the halves
+        ListNode* first = head;
+        ListNode* second = newHead;
+
+        while(second!=NULL){
+            if(first->val != second->val){
+                //returning back to the original config of LL
+                    reverseLL(newHead);
+                    return false;
+                }
+                first = first->next;
+                second = second->next;
+            }
+            //returning back to the original config of LL
+            reverseLL(newHead);
+            return true;
+        }
+
     
-    //Function to reverse LL
-    ListNode* reverseLL(ListNode* head){
+    ListNode* reverseLL(ListNode* head) {
         
-        ListNode* prev = NULL;
-        ListNode* next_node = NULL;
-        ListNode* curr = head;
-        
-        while(curr!=NULL){
-            next_node = curr->next;
-            curr->next = prev;
-            prev = curr;
-            curr = next_node;
+        ListNode* nextPtr;
+        ListNode* currPtr = head;
+        ListNode* prevPtr = NULL;
+
+        //base case..
+        if(head == NULL || head->next == NULL){
+            return head;
         }
-        return prev;
+
+        while(currPtr!=NULL){
+            nextPtr = currPtr->next;
+            currPtr->next = prevPtr;
+            prevPtr = currPtr;
+            currPtr = nextPtr;
+        }
+        return prevPtr;
     }
+
 };
+        
