e4103658c2d3546de4f93534c1f6d916bfbf9dab
Varad Kulkarni <56363407+kvarad6@users.noreply.github.com>
2023-12-31 09:27:52 +0530

    Update 169_Majority Element.cpp | Leetcode | Arrays

--- arrays/169_Majority Element.cpp
+++ arrays/169_Majority Element.cpp
@@ -1,26 +1,28 @@
+//-------------- Approach 1: -----------------------//
+
+//element occurs more than n/2 times -> always occupy middle position
+//Sort the array -> find middle element
+
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
 
+        int n = nums.size();
+        return nums[n/2];
+    }
+};
 
-        //-------------- Approach 1: -----------------------//
+//---------------- Approach 2 ----------------------//
+//Using Hashmap | unordered_map
+//TC: O(NlogN)
+//SC: O(1)
+//fill the occurrence of each element -> transverse & see if for any element, count > n/2
 
-        //element occurs more than n/2 times -> always occupy middle position
-        //Sort the array -> find middle element
-
-        // sort(nums.begin(), nums.end());
-
-        // int n = nums.size();
-        // return nums[n/2];
-
-        //---------------- Approach 2 ----------------------//
-
-        //Using Hashmap | unordered_map
-        //TC: O(NlogN)
-        //SC: O(1)
-
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
         unordered_map<int, int> mpp;
-
         int n = nums.size();
 
         //record frequency of elements..
@@ -29,16 +31,47 @@ public:
         }
 
         //check if element occurs more than n/2 times..
-
-        n = n/2;
+        n=n/2;
         for(auto x: mpp){
             if(x.second>n){
                 return x.first;
             }
         }
         return 0;
+    }
+};
 
-        //---------------- Approach 3 --------------------//
+
+//---------------- Approach 3 ----------------------//
+//Using Hashmap 
+//TC: O(N)
+//SC: O(1)
+
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int count = 0;
+        int element = nums[0];
+
+        for(int i=0;i<nums.size();i++){
+            if(count==0){
+                element = nums[i];
+            }
+
+            if(element == nums[i]){
+                count++;
+            }
+            else{
+                count--;
+            }
+        }
+        return element;
+    }
+};
+
+
+
+        //---------------- Approach 4 --------------------//
 
         //Moore Voting Algorithm -> pending...
 
