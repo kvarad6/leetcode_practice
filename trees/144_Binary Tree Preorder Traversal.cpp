5c14a74522305c8b09069b9b157c9dacad8f7604
Varad Kulkarni <56363407+kvarad6@users.noreply.github.com>
2024-01-03 01:28:01 +0530

    Create 144_Binary Tree Preorder Traversal.cpp | Leetcode | Trees

---
+++ trees/144_Binary Tree Preorder Traversal.cpp
@@ -0,0 +1,60 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+ //--------------- Approach 1 ------------------//
+ //Recursive Traversal
+
+class Solution {
+public:
+
+    void fill(TreeNode* root, vector<int> &ans){
+        if(root==NULL){
+            return;
+        }
+        ans.push_back(root->val);
+        fill(root->left, ans);
+        fill(root->right, ans);
+    }
+
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int> ans;
+        fill(root, ans);
+        return ans;
+    }
+};
+
+//-------------- Approach 2 ---------------//
+//Iterative Traversal
+
+class Solution {
+public:
+
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int> ans;
+        stack<TreeNode*> st;
+        while(root || !st.empty()){
+            if(root!=NULL){
+                st.push(root);
+                ans.push_back(root->val);
+                root = root->left;
+            }
+            else{
+                root = st.top();
+                st.pop();
+                root = root->right;
+            }
+        }
+        return ans;
+    }
+};
+
+//pending: morris alg..
