a67144d755a3271211f6efaaf56ec0b031ba72a1
Varad Kulkarni <56363407+kvarad6@users.noreply.github.com>
2024-01-12 07:58:47 +0530

    Update 110_Balanced Binary Tree.cpp | Leetcode | Trees

--- trees/110_Balanced Binary Tree.cpp
+++ trees/110_Balanced Binary Tree.cpp
@@ -9,8 +9,33 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+ //-------------- Approach 1 ---------------//
+ //Bruite force
+
 class Solution {
 public:
+
+    int findHeight(TreeNode* root){
+        if(root==NULL){
+            return 0;
+        }
+        int leftHeight = findHeight(root->left);
+        //if at any point, this call returns -1, then return -1 as tree is not balanced
+        if(leftHeight==-1){
+            return -1;
+        }
+
+        int rightHeight = findHeight(root->right);
+        //if at any point, this call returns -1, then return -1 as tree is not balanced
+        if(rightHeight==-1){
+            return -1;
+        }
+
+        if(abs(leftHeight-rightHeight)>1){
+            return -1;
+        }
+        return 1 + max(leftHeight, rightHeight);
+    }
     bool isBalanced(TreeNode* root) {
         int ans = findHeight(root);
         if(ans!=-1){
@@ -18,26 +43,32 @@ public:
         }
         return false;
     }
+};
 
-    int findHeight(TreeNode* node){
-        //base case 
-        if(node==NULL){
+
+//---------------- Approach 2 ----------------//
+//optimised
+
+class Solution {
+public:
+
+    int findHeight(TreeNode* root){
+        if(root==NULL){
             return 0;
         }
-        int leftHeight = findHeight(node->left);
-        //if at any point, this call returns -1, then return -1 as tree is not balanced
-        if(leftHeight == -1){
+        int leftHeight = findHeight(root->left);
+        int rightHeight = findHeight(root->right);
+
+        if(abs(leftHeight-rightHeight)>1 || leftHeight==-1 || rightHeight==-1){
             return -1;
         }
-        int rightHeight = findHeight(node->right);
-        //if at any point, this call returns -1, then return -1 as tree is not balanced
-        if(rightHeight == -1){
-            return -1;
+        return 1 + max(leftHeight, rightHeight);
+    }
+    bool isBalanced(TreeNode* root) {
+        int ans = findHeight(root);
+        if(ans!=-1){
+            return true;
         }
-        
-        if(abs(leftHeight-rightHeight)>1){
-            return -1;
-        }
-        return max(leftHeight, rightHeight) + 1;
+        return false;
     }
 };
