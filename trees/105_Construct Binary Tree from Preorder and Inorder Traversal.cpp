d253ccb6e816f4eceb7e73c71a85b39b77542a08
Varad Kulkarni <56363407+kvarad6@users.noreply.github.com>
2024-01-16 17:36:00 +0530

    Update 105_Construct Binary Tree from Preorder and Inorder Traversal.cpp | Leetcode | Trees

--- trees/105_Construct Binary Tree from Preorder and Inorder Traversal.cpp
+++ trees/105_Construct Binary Tree from Preorder and Inorder Traversal.cpp
@@ -11,36 +11,32 @@
  */
 class Solution {
 public:
-    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+   TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+        map<int,int> mp;
         //declaring a hashmap and putting the inorder traversal in the hashmap to find the root optimally.
-        map<int, int> inMap;
-        for(int i=0;i<inorder.size();i++){
-            inMap[inorder[i]] = i;
+        for(int i=0;i<preorder.size();i++){
+            mp[inorder[i]]=i;
         }
-
-        TreeNode* root = buildTree(preorder, 0, preorder.size()-1, inorder, 0, inorder.size()-1, inMap);
-
+        TreeNode* root = construct(preorder,0,preorder.size()-1,inorder,0,inorder.size()-1,mp);
         return root;
     }
 
-    TreeNode* buildTree(vector<int>& preorder, int preStart, int preEnd, vector<int>& inorder, int inStart, int inEnd, map<int, int> inMap){
+    TreeNode* construct(vector<int>&preorder, int preStart, int preEnd, vector<int> &inorder,int inStart, int inEnd, map<int,int> &mp){
+
         //As long as we have some preorder/postorder
-        if(preStart > preEnd || inStart > inEnd){
-            return NULL;
-        } 
+        if(preStart>preEnd || inStart>inEnd) return NULL;
 
         //Creating a root
         //Starting element of the preorder will be the root
         TreeNode* root = new TreeNode(preorder[preStart]);
 
         //Figure out the inroot from map and the no. of left subtree elements..
-        int inRoot = inMap[root->val];
-        int numsLeft = inRoot - inStart;
+        int inRoot = mp[root->val];
+        int numsLeft = inRoot-inStart;
 
         //Use recursion to find left, right subtree roots in order to build the same.
-        root->left = buildTree(preorder, preStart + 1, preStart + numsLeft, inorder, inStart, inRoot-1, inMap);
-        root->right = buildTree(preorder, preStart + numsLeft + 1, preEnd, inorder, inRoot+1, inEnd, inMap);
-
+         root->left = construct(preorder,preStart+1,preStart+numsLeft,inorder,inStart,inRoot-1,mp);
+        root->right = construct(preorder,preStart+numsLeft+1,preEnd,inorder,inRoot+1,inEnd,mp);
         return root;
     }
 
