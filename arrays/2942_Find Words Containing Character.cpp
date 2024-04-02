f00bbe0536cf04ab5207be1129091a78b78739c2
Varad Kulkarni <56363407+kvarad6@users.noreply.github.com>
2024-04-02 10:40:22 +0530

    Update 2942_Find Words Containing Character.cpp | Arrays | String

--- arrays/2942_Find Words Containing Character.cpp
+++ arrays/2942_Find Words Containing Character.cpp
@@ -1,10 +1,12 @@
+//-------------- Approach 1 --------------//
+// Bruite force
+// TC: O(N^2)
+
 class Solution {
 public:
     bool findChar(string word, char x){
         for(int i=0;i<word.size();i++){
-            cout<<word[i];
-            cout<<x;
-            if(word[i] == x){
+            if(word[i]==x){
                 return true;
             }
         }
@@ -13,9 +15,34 @@ public:
     vector<int> findWordsContaining(vector<string>& words, char x) {
         vector<int> ans;
         for(int i=0;i<words.size();i++){
-           if(findChar(words[i], x)==true){
-               ans.push_back(i);
-           }
+            if(findChar(words[i], x)==true){
+                ans.push_back(i);
+            }
+        }
+        return ans;
+    }
+};
+
+//------------- Approach 2 -------------//
+// Using npos | until end of a string
+// returns -1 if no match
+// TC: ~O(N)
+
+class Solution {
+public:
+    bool findChar(string word, char x){
+        int found = word.find(x);
+        if(found!=string::npos){
+            return true;
+        }
+        return false;
+    }
+    vector<int> findWordsContaining(vector<string>& words, char x) {
+        vector<int> ans;
+        for(int i=0;i<words.size();i++){
+            if(findChar(words[i], x)==true){
+                ans.push_back(i);
+            }
         }
         return ans;
     }
