72c5010ce0ae73e768a2b87d020ae8025d610120
Varad Kulkarni <56363407+kvarad6@users.noreply.github.com>
2024-04-03 22:20:19 +0530

    Update 2108_Find First Palindromic String in the Array.cpp | String

--- string/2108_Find First Palindromic String in the Array.cpp
+++ string/2108_Find First Palindromic String in the Array.cpp
@@ -1,18 +1,49 @@
+//------------ Approach 1 ------------//
+// Using reverse keyword
+
 class Solution {
 public:
-    bool isPalidrome(string word){
-        string reverseWord = word;
+    bool checkPalidrome(string word){
+        string reverseWord=word;
         reverse(word.begin(), word.end());
-        if(word == reverseWord){
+        if(word==reverseWord){
             return true;
         }
-        else{
-            return false;
-        }
+        return false;
     }
     string firstPalindrome(vector<string>& words) {
         for(int i=0;i<words.size();i++){
-            if(isPalidrome(words[i])){
+            if(checkPalidrome(words[i])){
+                return words[i];
+            }
+        }
+        return "";
+    }
+};
+
+//------------ Approach 2 ------------//
+// Using two pointers
+
+class Solution {
+public:
+    bool checkPalidrome(string word){
+        int i=0;
+        int j=word.size()-1;
+
+        while(i<=j){
+            if(word[i]==word[j]){
+                i++;
+                j--;
+            }
+            else{
+                return false;
+            }
+        }
+        return true;
+    }
+    string firstPalindrome(vector<string>& words) {
+        for(int i=0;i<words.size();i++){
+            if(checkPalidrome(words[i])){
                 return words[i];
             }
         }
