b69a7180169bda31147815ec5e7c0e66add3c761
Varad Kulkarni <56363407+kvarad6@users.noreply.github.com>
2024-04-10 10:35:11 +0530

    Update 2540_Minimum Common Value.cpp | Two Pointers | Set

--- arrays/2540_Minimum Common Value.cpp
+++ arrays/2540_Minimum Common Value.cpp
@@ -1,10 +1,33 @@
-//--------------- Approach 1 -------------//
-// Using two pointers
+//------- Approach 1 -------//
+//Using Set
+class Solution {
+public:
+    int getCommon(vector<int>& nums1, vector<int>& nums2) {
+        set<int> st;
+        //inserting nums1 element into the set
+        for(auto it: nums1){
+            st.insert(it);
+        }
+
+        //inserting nums2 element into the set 
+        //if common element found, return 
+
+        for(auto it: nums2){
+            if(st.count(it)!=0){
+                return it;
+            }
+        }
+        return -1;
+    }
+};
+
+//-------- Approach 2 --------//
+//Using two pointers
 
 class Solution {
 public:
     int getCommon(vector<int>& nums1, vector<int>& nums2) {
-        int i=0, j=0;
+       int i=0, j=0;
         while(i<nums1.size() && j<nums2.size()){
             if(nums1[i]==nums2[j]){
                 return nums1[i];
