0463a9353ecc99f0bc6cc99f376f3fa388332f80
Varad Kulkarni <56363407+kvarad6@users.noreply.github.com>
2024-05-08 18:20:13 +0530

    Update 58_Length of Last Word.py | string

--- string/58_Length of Last Word.py
+++ string/58_Length of Last Word.py
@@ -1,3 +1,5 @@
+#-------- Method 1 ---------#
+# to remove trailing spaces and traverse from back
 #.strip() -> used to remove leading and treailing white spaces..
 class Solution:
     def lengthOfLastWord(self, s: str) -> int:
@@ -9,6 +11,18 @@ class Solution:
                 break
             length+=1
         return length
+
+
+#-------------- Method 2 -----------#
+# strip -> split in words -> if there is not word, return 0, else return last word length 
+class Solution:
+    def lengthOfLastWord(self, s: str) -> int:
+        words = s.strip().split()
+
+        if not words:
+            return 0
+
+        return len(words[len(words)-1]) # can write len(words[-1]) instead
 
 
         
