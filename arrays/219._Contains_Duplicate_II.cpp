b2a9202e3e300244033fc0cf844e24d035afa9aa
Varad Kulkarni <56363407+kvarad6@users.noreply.github.com>
2024-05-16 17:19:11 +0530

    Create 219._Contains_Duplicate_II.cpp | Arrays | Hashmap

---
+++ arrays/219._Contains_Duplicate_II.cpp
@@ -0,0 +1,51 @@
+//------------ Approach 1 ------------//
+// bruite force
+// TC: O(N^2)
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        int i = 0;
+        int j = nums.size()-1;
+        int difference = 0;
+        for(int i=0;i<nums.size();i++){
+            for(int j=i+1;j<nums.size();j++){
+                if(nums[i]==nums[j]){
+                    if(abs(i-j)<=k){
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+};
+
+//---------- Approach 2 ----------//
+//using hashmap
+//TC: O(N)
+//SC: O(N)
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        unordered_map<int, int> mpp;
+        for(int i=0;i<nums.size();i++){
+            //to check if element found before
+            if(mpp.count(nums[i])!=NULL){
+                //check for condition
+                if(abs(i-mpp[nums[i]])<=k){
+                    return true;
+                }
+            }
+            //if element not covered before, insert the same
+            // and if the number is already present in the map, then update the position of that number
+            mpp[nums[i]] = i;
+        }
+        return false;
+    }
+};
+
+//---------- Approach 3 ----------//
+//sliding window
+
+
+
