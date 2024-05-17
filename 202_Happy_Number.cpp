28d8d2d4d7cbdb78ed910a0f66586dc7dfcd0ac0
Varad Kulkarni <56363407+kvarad6@users.noreply.github.com>
2024-05-17 21:55:23 +0530

    Create 202_Happy_Number.cpp | Math | Hashset

---
+++ 202_Happy_Number.cpp
@@ -0,0 +1,30 @@
+//------------ Approach 1 ------------//
+// Hashset (to keep record of completed nums and avoid infinite loop) + Math (n%10 and n/10)
+//TC: O(logN)
+//SC: to be determined | Hash Table(unordered_set(set)) space
+
+class Solution {
+public:
+    int nextNumber(int n){
+        int newNumber = 0;
+        while(n!=0){
+            int num = n%10;
+            newNumber += num*num;
+            n = n/10;
+        }
+        return newNumber;
+    }
+
+    bool isHappy(int n) {
+        unordered_set<int> set;
+        while(n!=1 && set.count(n)==NULL){
+            set.insert(n);
+            n = nextNumber(n);
+        }
+        return n==1 ? true:false;
+    }
+};
+
+//------------ Approach 1 ------------//
+//Floyd's Cycle-Finding Algorithm
+//pending
