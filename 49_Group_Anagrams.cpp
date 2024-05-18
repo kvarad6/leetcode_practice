c7bcf68a84692f0fa90fc1910327c13e898782a1
Varad Kulkarni <56363407+kvarad6@users.noreply.github.com>
2024-05-18 07:26:47 +0530

    Create 49_Group_Anagrams.cpp | Array | String | Hashmap

---
+++ 49_Group_Anagrams.cpp
@@ -0,0 +1,20 @@
+//------------ Approach 1 ------------//
+//strings are anagrams <=> same string after sorting. No need for frequency count.
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, vector<string>> mpp;
+        
+        for(auto x : strs){
+            string word = x;
+            sort(word.begin(), word.end());
+            mpp[word].push_back(x);
+        }
+
+        vector<vector<string>> ans;
+        for(auto x: mpp){
+            ans.push_back(x.second);
+        }
+        return ans;
+    }
+};
