6b64fce339b050a183c21a2486ef548e4a95a523
Varad Kulkarni <56363407+kvarad6@users.noreply.github.com>
2024-05-18 06:24:16 +0530

    Update 202_Happy_Number.cpp | Math | Hashset | Floyd's Cycle-Finding Alg

--- 202_Happy_Number.cpp
+++ 202_Happy_Number.cpp
@@ -25,6 +25,30 @@ public:
     }
 };
 
-//------------ Approach 1 ------------//
+//------------ Approach 2 ------------//
 //Floyd's Cycle-Finding Algorithm
-//pending
+//using fast-slow pointer
+
+class Solution {
+public:
+    int nextNumber(int n){
+        int newNumber = 0;
+        while(n!=0){
+            int num = n%10;
+            newNumber += num*num;
+            n = n/10;
+        }
+        return newNumber;
+    }
+
+    bool isHappy(int n) {
+        int fastPointer = nextNumber(n);
+        int slowPointer = n;
+
+        while(fastPointer!=1 && fastPointer!=slowPointer){
+            slowPointer = nextNumber(slowPointer);
+            fastPointer = nextNumber(nextNumber(fastPointer));
+        }
+        return fastPointer==1 ? true:false;
+    }
+};
