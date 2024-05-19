639fccbd7060e4d121a906cee3cca9d120287679
Varad Kulkarni <56363407+kvarad6@users.noreply.github.com>
2024-05-19 07:55:09 +0530

    Create 134_Gas_Station.cpp | Arrays | Greedy | Kadane's Alg

---
+++ arrays/134_Gas_Station.cpp
@@ -0,0 +1,36 @@
+//------------ Approach 1 ------------//
+//Using kadane's alg | Greedy
+//first step: check for total gas and total cost of the journey
+//if total gas >= total cost -> journey possible
+
+//second step -> to identify starting point, just check if [gas at that station - cost] required at that station,
+//if its positive, can start from that station, otherwise go to next station
+class Solution {
+public:
+    int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+        //step 1
+        int totalGas = 0, totalCost=0;
+        for(int i=0;i<gas.size();i++){
+            totalGas += gas[i];
+            totalCost += cost[i];
+        }
+
+        if(totalGas<totalCost){
+            return -1;
+        }
+
+        //step 2
+
+        int startingPoint = 0, currentGas=0;
+
+        for(int i=0; i<gas.size();i++){
+            currentGas += gas[i] - cost[i];
+            if(currentGas < 0){
+                currentGas = 0;
+                startingPoint = i+1;
+            }
+        }
+
+        return startingPoint;
+    }
+};
