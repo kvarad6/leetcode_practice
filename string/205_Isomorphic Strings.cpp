36bad276a120efdd22ef1deb3447085eb53b1392
Varad Kulkarni <56363407+kvarad6@users.noreply.github.com>
2024-05-21 11:42:24 +0530

    Update 205_Isomorphic Strings.cpp | String | Hashmap

--- string/205_Isomorphic Strings.cpp
+++ string/205_Isomorphic Strings.cpp
@@ -1,3 +1,5 @@
+//----------- Approach 1 -----------//
+
 class Solution {
 public:
     bool isIsomorphic(string s, string t) {
@@ -25,5 +27,43 @@ public:
         }      
         }
         return true;
+    }
+};
+
+
+//------------ Approach 2 ------------//
+//Using two unordered maps
+//mpp1 -> mapping of s->t
+//mpp2 -> mapping of t->s
+//In case of isomorphic strings, both mappings should be equal
+
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        unordered_map<char, char> mpp1;
+        unordered_map<char, char> mpp2;
+
+        for(int i=0;i<s.size();i++){
+            //check s->t
+            if(mpp1.find(s[i])!=mpp1.end()){
+                if(mpp1[s[i]]!=t[i]){
+                    return false;
+                }
+            }
+
+            //check t->s
+            if(mpp2.find(t[i])!=mpp2.end()){
+                if(mpp2[t[i]]!=s[i]){
+                    return false;
+                }
+            }
+            //map s->t if not exist
+            mpp1[s[i]] = t[i];
+
+            //map t->s if not exist
+            mpp2[t[i]] = s[i];
+        }
+        
+       return true;
     }
 };
