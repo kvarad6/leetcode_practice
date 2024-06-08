8819377d622d3a5c8718f5b862caa1b6f6e6afcb
Varad Kulkarni <56363407+kvarad6@users.noreply.github.com>
2024-06-08 09:39:08 +0530

    Update 55_Jump_Game.cpp | Array | Greedy

--- arrays/55_Jump_Game.cpp
+++ arrays/55_Jump_Game.cpp
@@ -22,4 +22,38 @@ public:
     }
 };
 
+//---------- Approach 2 -----------//
+//at each position, to check if by adding value at that position with index combined can reach to the end
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+       //base case
+       if(nums.size()==1){
+        return true;
+       }
+       int maxReachIndex = nums[0];
+
+       for(int i=0;i<nums.size();i++){
+        //condition 1
+        //if maxReach is at last index, return true
+        if(maxReachIndex>=nums.size()-1){
+            return true;
+        }
+
+        //condition 2
+        //if encountered 0 and its not at the last index, you're stuck, return false
+        if(nums[i]==0 && i==maxReachIndex){
+            return false;
+        }
+
+        //condition 3
+        //if maxReach is less than currentReach at specific index, update maxReach
+        int currentReachIndex = i + nums[i];
+        maxReachIndex = max(maxReachIndex, currentReachIndex);
+       }
+       return false;        
+    }
+};
+
+
 //peeding: DP
