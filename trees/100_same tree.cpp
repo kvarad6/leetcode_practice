be262d919bad626128bf0f513db6a7ab04e42e53
Varad Kulkarni <56363407+kvarad6@users.noreply.github.com>
2024-06-09 13:34:48 +0530

    Update 100_same tree.cpp | Tree | BFS | DFS

--- trees/100_same tree.cpp
+++ trees/100_same tree.cpp
@@ -9,24 +9,75 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+//----------- Approach 1 -----------//
+//Using DFS | Recursion
 class Solution {
 public:
     bool isSameTree(TreeNode* p, TreeNode* q) {
-        //base cases
+        //edge cases
         //if both trees are null, they are identical
-        if(p == NULL && q == NULL){
+        if(p==NULL && q==NULL){
             return true;
         }
 
         //if only one of the trees is null, they are not identical 
-        if(p == NULL || q == NULL){
+        if(p==NULL || q==NULL){
             return false;
         }
-        //if the root nodes of both the trees are equal, recursively check if the left and right sbtrees are identical
 
-        if(p->val == q->val){
-            return isSameTree(p->left, q->left)  && isSameTree(p->right, q->right);
+        //if the root nodes of both the trees are equal, recursively check if the left and right sbtrees are identical
+        if(p->val==q->val){
+            return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
         }
-        return false;
+        return false;   
+    }
+};
+
+
+//----------- Approach 2 -----------//
+//Using BFS | queue
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        //edge cases
+        if(p==NULL && q==NULL){
+            return true;
+        }
+
+        if(p==NULL || q==NULL){
+            return false;
+        }  
+
+        queue<TreeNode*> q1;
+        queue<TreeNode*> q2;
+
+        q1.push(p);
+        q2.push(q);
+
+        while(!q1.empty() && !q2.empty()){
+            TreeNode* node1 = q1.front();
+            TreeNode* node2 = q2.front();
+
+            q1.pop();
+            q2.pop();
+
+            if(node1==NULL || node2==NULL){
+                if(node1!=node2){
+                    return false;
+                }
+                continue;
+            }
+
+            if(node1->val != node2->val){
+                return false;
+            }
+            q1.push(node1->left);
+            q1.push(node1->right);
+
+            q2.push(node2->left);
+            q2.push(node2->right);
+        }
+
+        return true;
     }
 };
