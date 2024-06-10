22516e702ab7ac727d3bb6d73d7c3c9cb271a197
Varad Kulkarni <56363407+kvarad6@users.noreply.github.com>
2024-06-10 09:27:45 +0530

    Update 101_Symmetric Tree.cpp | Tree | DFS | BFS

--- trees/101_Symmetric Tree.cpp
+++ trees/101_Symmetric Tree.cpp
@@ -9,6 +9,9 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
+//------------ Approach 1 -------------//
+// DFS | Recursion 
 class Solution {
 public:
     bool isSymmetric(TreeNode* root) {
@@ -37,4 +40,54 @@ public:
 
         return isSymmetricHelp(left->left, right->right) && isSymmetricHelp(left->right, right->left);
     } 
+};
+
+
+//----------- Approach 2 ------------//
+// BFS | Queues
+
+class Solution {
+public:
+    bool isSymmetric(TreeNode* root) {
+        if(root==NULL){
+            return true;
+        }
+
+        if(root->left==NULL && root->right==NULL){
+            return true;
+        }
+
+        queue<TreeNode*> q1;
+        queue<TreeNode*> q2;
+        q1.push(root->left);
+        q2.push(root->right);
+
+        while(!q1.empty() && !q2.empty()){
+            TreeNode* left = q1.front();
+            q1.pop();
+
+            TreeNode* right = q2.front();
+            q2.pop();
+
+            if(left==NULL && right==NULL){
+                continue;
+            }
+
+            if(left==NULL || right==NULL){
+                return false;
+            }
+
+            if(left->val!=right->val){
+                return false;
+            }
+
+            q1.push(left->left);
+            q2.push(right->right);
+
+            q1.push(left->right);
+            q2.push(right->left);
+            
+        }
+        return true;
+    }
 };
