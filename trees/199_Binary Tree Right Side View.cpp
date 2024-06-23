e6e8051859ea576913dd0dced5834ba901f834d0
Varad Kulkarni <56363407+kvarad6@users.noreply.github.com>
2024-06-23 10:06:03 +0530

    Update 199_Binary Tree Right Side View.cpp | Tree | BFS | DFS

--- trees/199_Binary Tree Right Side View.cpp
+++ trees/199_Binary Tree Right Side View.cpp
@@ -10,32 +10,67 @@
  * };
  */
 
-//--------------------- Approach 1 ------------------------//
-// Recursive solution
-// Reverse Preorder Traversal | Root->Right->Left
+//----------- Approach 1 ----------//
+//Using BFS | queue
+//Adding the last node in each level to the result
+class Solution {
+public:
+    vector<int> rightSideView(TreeNode* root) {
+        queue<TreeNode*> q;
+        vector<int> view;
+
+        if(root==NULL){
+            return view;
+        }
+
+        q.push(root);
+
+        while(!q.empty()){
+            int queueSize = q.size();
+            for(int i=0;i<queueSize;i++){
+                TreeNode* node = q.front();
+                q.pop();
+                
+                //pushing last element of each level to the array
+                if(i==queueSize-1){
+                    view.push_back(node->val);
+                }
+                if(node->left!=NULL){
+                    q.push(node->left);
+                }
+                if(node->right!=NULL){
+                    q.push(node->right);
+                }
+            }
+        }
+        return view;
+    }
+};
+
+//-------- Approach 2 ---------/
+//Using DFS | Recursion
 
 class Solution {
 public:
-   void recursive_traversal(TreeNode* root, int level, vector<int> &res){
-        
+
+    void rightView(TreeNode* root, vector<int> &view, int level){
+
         if(root==NULL){
             return;
         }
-        
-        if(res.size() == level){
-            res.push_back(root->val);
-             }
-        recursive_traversal(root->right, level+1, res);
-        recursive_traversal(root->left, level+1, res);
-       
+
+        if(view.size()==level){
+            view.push_back(root->val);
+        }
+
+        rightView(root->right, view, level+1);
+        rightView(root->left, view, level+1);
     }
-    
     vector<int> rightSideView(TreeNode* root) {
-        vector<int> res;
         int level = 0;
-        recursive_traversal(root, level, res);
-        return res;
+        vector<int> view;
+
+        rightView(root, view, level);
+        return view;
     }
-     
-   
 };
