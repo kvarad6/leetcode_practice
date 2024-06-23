178eb8d4ac3806c9faae171b5299f234b2189094
Varad Kulkarni <56363407+kvarad6@users.noreply.github.com>
2024-06-23 13:16:59 +0530

    Update 103_Binary Tree Zigzag Level Order Traversal.cpp | Tree | BFS

--- trees/103_Binary Tree Zigzag Level Order Traversal.cpp
+++ trees/103_Binary Tree Zigzag Level Order Traversal.cpp
@@ -9,7 +9,7 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-
+//----------- Approach 1 ------------//
  // zigzag traversal = BFS (level order traversal) + flag for odd level
 class Solution {
 public:
@@ -50,3 +50,61 @@ public:
         return ans;
     }
 };
+
+
+//----------- Appraoch 2 ------------//
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+//----------- Approach 2 ---------//
+//Using BFS | queue
+class Solution {
+public:
+    vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
+        vector<vector<int>> ans;
+
+        if(root==NULL){
+            return ans;
+        }
+
+        queue<TreeNode*> q;
+        q.push(root);
+        int count = 0;
+
+        while(!q.empty()){
+            int size = q.size();
+            vector<int> level;
+            for(int i=0;i<size;i++){
+                TreeNode* node = q.front();
+                q.pop();
+                level.push_back(node->val);
+                if(node->left!=NULL){
+                    q.push(node->left);
+                }
+                if(node->right!=NULL){
+                    q.push(node->right);
+                }
+            }
+            if(count%2==0){
+                ans.push_back(level);
+            }
+            else{
+                reverse(level.begin(), level.end());
+                ans.push_back(level);
+            }
+            count++;
+        }
+        return ans;
+    }
+};
+
+//----- PENDING : DFS Approach ---------//
