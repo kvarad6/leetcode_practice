865913a0b1d3a96389dafc35d0a310f4676a7f62
Varad Kulkarni <56363407+kvarad6@users.noreply.github.com>
2024-08-02 00:09:24 +0530

    Update 235_Lowest_Common_Ancestor_of_a_Binary_Search_Tree.cpp | Tree

--- trees/235_Lowest Common Ancestor of a Binary Search Tree.cpp
+++ trees/235_Lowest Common Ancestor of a Binary Search Tree.cpp
@@ -8,6 +8,51 @@
  * };
  */
 
+//---------------- Approach 1 --------------------//
+
+/*
+Various conditions that to be handled:
+lets 'a' and 'b' are two nodes whose lowest common ancessator to be find out
+1. if current node is a or b
+2. if 'a' is in left subtree and 'b' is in right subtree
+3. both 'a' and 'b' is any one subtree
+4. None of 'a' and 'b' in any subtree
+*/
+
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        //edge case
+        if(root==NULL){
+            return NULL;
+        }
+        //if current node/ root is p/q, then it itself a lowest ancestor
+        if(root==p || root==q){
+            return root;
+        }
+
+        //check if you find anything in left subtree
+        TreeNode* leftSideNode = lowestCommonAncestor(root->left, p, q);
+        //transverse right subtree
+        TreeNode* rightSideNode = lowestCommonAncestor(root->right, p, q);
+
+        //if left subtree does not return anything/ p/q not present in left subtree
+        if(leftSideNode==NULL){
+            return rightSideNode;
+        }
+        //if right subtree does not return anything/ p/q not present in right subtree
+        if(rightSideNode==NULL){
+            return leftSideNode;
+        }
+        //if we found one among p/q in left subtree and other in right subtree, then the current node is lowest common ancestor
+        return root;
+    }
+};
+
+
+
+
+//------------------ Approach 2 ------------------//
 class Solution {
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
