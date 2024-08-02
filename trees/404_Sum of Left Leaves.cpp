ced5f89555e5832e0a66cbfee6e47105c8ed8055
Varad Kulkarni <56363407+kvarad6@users.noreply.github.com>
2024-08-03 00:28:24 +0530

    Update 404_Sum of Left Leaves.cpp | Tree | BFS | DFS

--- trees/404_Sum of Left Leaves.cpp
+++ trees/404_Sum of Left Leaves.cpp
@@ -9,24 +9,59 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+//----------- Approach 1 -----------//
+// DFS | Recursion
 class Solution {
 public:
-int sum = 0;
-    int sumOfLeftLeaves(TreeNode* root) {
-        sumHelper(root, false);
-        return sum;
+    void helper(TreeNode* root, int& sum, bool isLeft){
+       
+        if(root==NULL){
+            return;
+        }
+        
+        if(isLeft==true && root->left==NULL && root->right==NULL){
+            sum+= root->val;
+        }
+
+        helper(root->left, sum, true);
+        helper(root->right, sum, false);
     }
 
-    void sumHelper(TreeNode* node, bool isLeft){
-        if(node==NULL){
-            return;
+    int sumOfLeftLeaves(TreeNode* root) {
+        int sum = 0;
+        bool isLeft = false;
+        helper(root, sum, isLeft);
+        return sum;
+    }
+};
+
+
+//---------- Approach 2 ----------//
+// BFS | Using queue
+
+class Solution {
+public:
+    int sumOfLeftLeaves(TreeNode* root) {
+        int sum = 0;
+        queue<TreeNode*> q;
+
+        q.push(root);
+        while(!q.empty()){
+            int size = q.size();
+            for(int i=0;i<size;i++){
+                TreeNode* node = q.front();
+                q.pop();
+                if(node->left!=NULL){
+                    if(node->left->left==NULL && node->left->right==NULL){
+                        sum += node->left->val;
+                    }
+                    q.push(node->left);
+                }
+                if(node->right!=NULL){
+                    q.push(node->right);
+                }
+            }
         }
-        //if node is a leaf node and a left node
-        if(isLeft && node->left == NULL && node->right == NULL){
-            sum += node->val;
-            return;
-        }
-        sumHelper(node->left, true);
-        sumHelper(node->right, false);
+        return sum;
     }
 };
