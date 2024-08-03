ead0803d9b712c96f7f538409a729f2fa4e1f473
Varad Kulkarni <56363407+kvarad6@users.noreply.github.com>
2024-08-03 13:59:37 +0530

    Update 543_Diameter of Binary Tree.cpp  | Tree | DFS

--- trees/543_Diameter of Binary Tree.cpp
+++ trees/543_Diameter of Binary Tree.cpp
@@ -9,34 +9,35 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-
+//------------- Approach 1 ---------------//
  // Brute force approach..
 
-// class Solution {
-// public:
-//     int diameterOfBinaryTree(TreeNode* root) {
-//         if(root==NULL){
-//             return 0;
-//         }
-//         int leftDiameter = diameterOfBinaryTree(root->left);
-//         int rightDiameter = diameterOfBinaryTree(root->right);
+class Solution {
+public:
+    int diameterOfBinaryTree(TreeNode* root) {
+        if(root==NULL){
+            return 0;
+        }
+        int leftDiameter = diameterOfBinaryTree(root->left);
+        int rightDiameter = diameterOfBinaryTree(root->right);
 
-//         int currentIncluded = height(root->left) + height(root->right);
+        int currentIncluded = height(root->left) + height(root->right);
 
-//         return max(currentIncluded, max(leftDiameter, rightDiameter));
-//     }
+        return max(currentIncluded, max(leftDiameter, rightDiameter));
+    }
 
-//     int height(TreeNode* root){
-//         if(root==NULL){
-//             return 0;
-//         }
-//         int leftHeight = height(root->left);
-//         int rightHeight = height(root->right);
+    int height(TreeNode* root){
+        if(root==NULL){
+            return 0;
+        }
+        int leftHeight = height(root->left);
+        int rightHeight = height(root->right);
 
-//         return 1 + max(leftHeight, rightHeight);
-//     }
-// };
+        return 1 + max(leftHeight, rightHeight);
+    }
+};
 
+//--------------- Approach 2 ----------------//
 //Optimal approach:
 
 class Solution {
@@ -58,6 +59,45 @@ public:
         diameter = max(diameter, leftHeight+rightHeight);
         return 1+max(leftHeight, rightHeight);
     }
+};
 
-   
+
+//---------------- Approach 3 ---------------------//
+// Calculating diameter for each node, keeping track of the longest one.
+// diameter = height of left tree + height of right tree
+/*
+    Finding max diameter from 3 possible cases:
+    Starting from root,
+    1. max diameter can be present in the left subtree
+        leftDiameter = diameterOfBinaryTree(root->left)
+
+    2. max diameter can be present in the right subtree
+        rightDiameter = diameterOfBinaryTree(root->right)
+
+    3. max diameter can pass through node
+        leftHeight = height(root->left)
+        rightHeight = height(root->right)
+        diameter = leftHeight + rightHeight + 1
+*/
+
+class Solution {
+public:
+    int findHeight(TreeNode* node){
+        if(node==NULL){
+            return 0;
+        }
+       return 1+max(findHeight(node->left), findHeight(node->right));
+    }
+
+    int diameterOfBinaryTree(TreeNode* root) {
+        if(root==NULL){
+            return 0;
+        }
+        int leftDiameter = diameterOfBinaryTree(root->left);
+        int rightDiameter = diameterOfBinaryTree(root->right);
+        int leftHeight = findHeight(root->left);
+        int rightHeight = findHeight(root->right);
+        int rootDiameter = leftHeight + rightHeight;
+        return max(rootDiameter, max(leftDiameter, rightDiameter));
+    }
 };
