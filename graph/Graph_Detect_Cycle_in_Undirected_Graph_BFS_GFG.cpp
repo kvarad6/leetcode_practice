dc0261f8fa67670e0cb6be2ccfb8e22cae803f7b
Varad Kulkarni <56363407+kvarad6@users.noreply.github.com>
2024-08-16 13:47:18 +0530

    Create Graph_Detect_Cycle_in_Undirected_Graph_BFS_GFG.cpp | BFS | GFG

---
+++ graph/Graph_Detect_Cycle_in_Undirected_Graph_BFS_GFG.cpp
@@ -0,0 +1,45 @@
+class Solution {
+  public:
+    // Function to detect cycle in an undirected graph.
+    bool helper(int source, vector<int> adj[], vector<int> &visited){
+        
+        //marking source node as visited
+        visited[source] = 1;
+        //declare a queue to store pair of current node and immediate parent
+        queue<pair<int, int>>q;
+        q.push({source, -1});
+        while(!q.empty()){
+            int node = q.front().first;
+            int parent = q.front().second;
+            q.pop();
+            
+            //transverse the adj list, and if the neighbour is not visited, add it in queue
+            for(auto adjElement : adj[node]){
+                if(visited[adjElement]==0){
+                    visited[adjElement]=1;
+                    q.push({adjElement, node});
+                }else if(parent!=adjElement){
+                    return true;
+                }
+            }
+            
+        }
+        return false;
+        
+    }
+    
+    bool isCycle(int V, vector<int> adj[]) {
+        vector<int> visited(V, 0);
+        //also need to handle if components are broken
+        //here, we are covering all the components (connected/ not-connected)
+        for(int i=1;i<V;i++){
+            //here, we are skipping all the nodes which are visited in a component (skipping the entire component who is one's covered)
+            if(!visited[i]){
+                if(helper(i, adj, visited)==true){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+};
