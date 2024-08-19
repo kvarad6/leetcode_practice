df79cde0f52935c340014283537ccfd62e911367
Varad Kulkarni <56363407+kvarad6@users.noreply.github.com>
2024-08-19 13:13:37 +0530

    Update 617_Merge_Two_Binary_Trees.cpp

--- trees/617_Merge_Two_Binary_Trees.cpp
+++ trees/617_Merge_Two_Binary_Trees.cpp
@@ -9,6 +9,8 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+//------------ Approach 1 ------------//
+//Inplace merging | without creating new tree | modifying root1
 class Solution {
 public:
     TreeNode* mergeTrees(TreeNode* root1, TreeNode* root2) {
@@ -25,5 +27,53 @@ public:
         root1->left = mergeTrees(root1->left, root2->left);
         root1->right = mergeTrees(root1->right, root2->right);
         return root1;
+    }
+};
+
+//----------- Approach 2 ---------------//
+//Creating new tree | using extra space
+class Solution {
+public:
+
+    TreeNode* mergeTrees(TreeNode* root1, TreeNode* root2) {
+        if(root1==NULL){
+            return root2;
+        }
+        if(root2==NULL){
+            return root1;
+        }
+        TreeNode* newRoot = new TreeNode(root1->val + root2->val);
+
+        newRoot->left = mergeTrees(root1->left, root2->left);
+        newRoot->right = mergeTrees(root1->right, root2->right);
+        
+        return newRoot;
+    }
+};
+
+//------------ Appraoch 3 ---------------//
+//Creating new tree | using extra space
+class Solution {
+public:
+
+    TreeNode* mergeTrees(TreeNode* root1, TreeNode* root2) {
+        if(root1==NULL && root2==NULL){
+            return NULL;
+        }
+        if(root1==NULL){
+            return root2;
+        }
+        if(root2==NULL){
+            return root1;
+        }
+
+        int val1 = root1?root1->val:0;
+        int val2 = root2?root2->val:0;
+        
+        // root1->val += root2->val;
+        TreeNode* newRoot = new TreeNode(val1 + val2);
+        newRoot->left = mergeTrees(root1?root1->left:NULL, root2?root2->left:NULL);
+        newRoot->right = mergeTrees(root1?root1->right:NULL, root2?root2->right:NULL);
+        return newRoot;
     }
 };
