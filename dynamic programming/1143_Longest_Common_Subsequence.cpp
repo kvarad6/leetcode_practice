72ad9459acaf4972ee702710d88f4756880a8d6f
Varad Kulkarni <56363407+kvarad6@users.noreply.github.com>
2024-08-30 14:21:14 +0530

    Update 1143_Longest_Common_Subsequence.cpp

--- dynamic programming/1143_Longest_Common_Subsequence.cpp
+++ dynamic programming/1143_Longest_Common_Subsequence.cpp
@@ -1,4 +1,66 @@
-//Recursion
+//-------- Recursion | Approach 1 | TLE ---------//
+class Solution {
+public:
+    int length = 0;
+    void helper(string text1, string text2, int i, int j){
+        if(i==text1.length() || j==text2.length()){
+            return;
+        }
+        if(text1[i]==text2[j]){
+            length++;
+            helper(text1, text2, i+1, j+1);
+        }else{
+            //saving the current length
+            int currentLength = length;
+
+            //moving in text2 using the helper function, store the length
+            helper(text1, text2, i, j+1);
+            int length1 = length;
+
+            //restoring the length, moving in text1, storing the length
+            length = currentLength;
+            helper(text1, text2, i+1, j);
+            int length2 = length;
+
+            length = max(length1, length2);
+        }
+    }
+
+    int longestCommonSubsequence(string text1, string text2) {
+        helper(text1, text2, 0, 0);
+        return length;
+    }
+};
+
+
+//-------- Recursion | Approach 2 | TLE ---------//
+class Solution {
+public:
+    int length = 0;
+    void helper(string text1, string text2, int i, int j, int currentLength){
+        length = max(length, currentLength);
+
+        if(i==text1.length() || j==text2.length()){
+            return;
+        }
+        if(text1[i]==text2[j]){
+            helper(text1, text2, i+1, j+1, currentLength+1);
+        }else{
+            helper(text1, text2, i, j+1, currentLength);
+            helper(text1, text2, i+1, j, currentLength);
+        }
+    }
+
+    int longestCommonSubsequence(string text1, string text2) {
+        int currentLength = 0;
+        helper(text1, text2, 0, 0, currentLength);
+        return length;
+    }
+};
+
+
+
+//---------- Recursion | Approach 3 | TLE ----------//
 class Solution {
     int solve(string &text1, string &text2, int i, int j){
         //base cases
