08a5815d700adca23258f31e00ad7bf30fbe2874
Varad Kulkarni <56363407+kvarad6@users.noreply.github.com>
2024-08-31 09:10:38 +0530

    Update Graph_Dijkstra's_Alg_Set_GFG.cpp

--- graph/Graph_Dijkstra's_Alg_Set_GFG.cpp
+++ graph/Graph_Dijkstra's_Alg_Set_GFG.cpp
@@ -12,7 +12,9 @@ class Solution
         dist[S] = 0;
         
         while(!st.empty()){
+	    //dereferencing the iterator pointer
             auto it = *(st.begin());
+	    //can also be written as: pair<int, int> it = *(st.begin());
             int node = it.second;
             int distance = it.first;
             st.erase(it);
@@ -21,9 +23,11 @@ class Solution
                 int adjWeight = adjElement[1];
                 //if we got the lessaer distance, than the one existed in the set, just erase the existed one to insert the updated smaller one
                 if(distance + adjWeight < dist[adjNode]){
+		    //if already some ditance present in the set for a specific node, we need to remove that if we want to add the node with less distance
                     if(dist[adjNode]!=1e9){
                         st.erase({dist[adjNode], adjNode});
                     }
+		    //else update the wt in distance array and insert into the set
                     dist[adjNode] = distance + adjWeight;
                     st.insert({dist[adjNode], adjNode});
                 }
