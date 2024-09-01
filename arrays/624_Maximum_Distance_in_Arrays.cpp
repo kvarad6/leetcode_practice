69c7af012d5d0d6617c95ed8f040290270fe1356
Varad Kulkarni <56363407+kvarad6@users.noreply.github.com>
2024-09-01 14:29:49 +0530

    Update 624_Maximum_Distance_in_Arrays.cpp

--- arrays/624_Maximum_Distance_in_Arrays.cpp
+++ arrays/624_Maximum_Distance_in_Arrays.cpp
@@ -1,15 +1,23 @@
-
 class Solution {
 public:
     int maxDistance(vector<vector<int>>& arrays) {
-        int maxDistance = 0;
+        //initially lets consider smallest and biggest from the first array
         int smallest = arrays[0][0];
         int biggest = arrays[0].back();
-        for(int i=1;i<arrays.size();i++){
-            maxDistance = max(maxDistance, abs(arrays[i].back() - smallest));
-            maxDistance = max(maxDistance, abs(biggest - arrays[i][0]));
-            smallest = min(arrays[i][0], smallest);
-            biggest = max(arrays[i].back(), biggest);
+        int maxDistance = INT_MIN;
+        //now trasverse the 2d array from second one
+        for(int i=1; i<arrays.size();i++){
+            //started by considering smallest of the first array and biggest of the current one, compairing and updating maxDistance
+            maxDistance = max(abs(arrays[i].back()-smallest), maxDistance);
+
+            //started by considering biggest of the first array and the smallest of the current one, compairing and updating maxDistance
+            maxDistance = max(abs(biggest-arrays[i][0]), maxDistance);
+
+            //update the smallest 
+            smallest = min(smallest, arrays[i][0]);
+
+            //update the biggest
+            biggest = max(biggest, arrays[i].back());
         }
         return maxDistance;
     }
