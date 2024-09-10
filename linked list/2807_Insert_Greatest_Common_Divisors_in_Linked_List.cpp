22a43a9df9f89441f9c8bb6b498a7ccab198d327
Varad Kulkarni <56363407+kvarad6@users.noreply.github.com>
2024-09-10 16:11:38 +0530

    Update 2807_Insert_Greatest_Common_Divisors_in_Linked_List.cpp

--- linked list/2807_Insert_Greatest_Common_Divisors_in_Linked_List.cpp
+++ linked list/2807_Insert_Greatest_Common_Divisors_in_Linked_List.cpp
@@ -8,6 +8,10 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+//----------- Appraoch 1 -----------//
+//Using in-built __gcd function
+//TC: O(n*log(m)) | n-> linkedlist size, m-> max value among all the elements in the linkedlist
+//SC: O(1)
 class Solution {
 public:
     ListNode* insertGreatestCommonDivisors(ListNode* head) {
@@ -19,6 +23,41 @@ public:
         ListNode* curr = head;
         while(curr->next!=NULL){
             ListNode* GCD = new ListNode(__gcd(curr->val, curr->next->val));
+            GCD->next = curr->next;
+            curr->next = GCD;
+            curr = GCD->next;
+        }
+        return head;
+    }
+};
+
+//----------Appraoch 2 -------------//
+//Calculating gcd manually
+//TC: O(n*m) | n-> linkedlist size, m-> min of two values
+//SC: O(1)
+class Solution {
+public:
+    int findGCD(int num1, int num2){
+        int result = min(num1, num2);
+        while(result>0){
+            if(num1%result==0 && num2%result==0){
+                break;
+            }
+            result--;
+        } 
+        return result;
+    }
+
+    ListNode* insertGreatestCommonDivisors(ListNode* head) {
+        //base cases
+        if(head==NULL || head->next==NULL){
+            return head;
+        }
+
+        ListNode* curr = head;
+        while(curr->next!=NULL){
+            int gcdValue = findGCD(curr->val, curr->next->val);
+            ListNode* GCD = new ListNode(gcdValue);
             GCD->next = curr->next;
             curr->next = GCD;
             curr = GCD->next;
