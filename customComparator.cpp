8d001dd2e06fbb196a6d95e02ab050ccc21f94da
Varad Kulkarni <56363407+kvarad6@users.noreply.github.com>
2024-09-18 14:15:28 +0530

    Create customComparator.cpp

---
+++ customComparator.cpp
@@ -0,0 +1,28 @@
+// Custom comparator
+#include <iostream>
+#include<vector>
+#include <bits/stdc++.h> 
+using namespace std; 
+
+bool customComparator(int a, int b){
+    if(a>b){
+        return true;
+    }else{
+        return false;
+    }
+    // return a>b;
+}
+
+int main() {
+    vector<int> arr;
+    for(int i=0;i<5;i++){
+        arr.push_back(i);
+    }
+    sort(arr.begin(), arr.end(), customComparator);
+    
+    for(int i=0;i<5;i++){
+        cout<<arr[i]<<" "; //4 3 2 1 0 
+    }
+
+    return 0;
+}
