eb5688aa2fde572941552889bb19ca6e34898362
Varad Kulkarni <56363407+kvarad6@users.noreply.github.com>
2024-09-30 16:44:41 +0530

    Update 1381_Design_a_Stack_With_Increment_Operation.cpp

--- 1381_Design_a_Stack_With_Increment_Operation.cpp
+++ 1381_Design_a_Stack_With_Increment_Operation.cpp
@@ -7,13 +7,15 @@ private:
     int count;
     int maxElements;
 public:
+    //TC: O(n)
     CustomStack(int maxSize) {
         // arr = vector<int>(maxSize);
         arr.reserve(maxSize);
         count = 0;
         maxElements = maxSize;
     }
-    
+
+    //TC: O(1)
     void push(int x) {
         if(count<maxElements){
             arr[count] = x;
@@ -21,7 +23,8 @@ public:
         }
         
     }
-    
+
+    //TC:O(1)
     int pop() { 
         if(count==0){
             return -1;
@@ -33,7 +36,8 @@ public:
         count--;
         return arr[count];
     }
-    
+
+    //TC: O(min(k, count))
     void increment(int k, int val) {
         int limit = min(k, count);
         for(int i=0;i<limit;i++){
